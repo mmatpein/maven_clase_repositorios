6bf700cb27bbf015b5c04cdc1fdbbbc98521e764
Mauricio Matamala Peinado <mauricio.politecnico@gmail.com>
2020-03-10 13:49:53 +0100

    Suma de double añadida

--- src/main/java/Suma.java
+++ src/main/java/Suma.java
@@ -1,5 +1,11 @@
+import java.security.PublicKey;
+
 public class Suma {
     public int sumar(int a, int b){
+        return a + b;
+    }
+
+    public double sumar(double a, double b){
         return a + b;
     }
 }
